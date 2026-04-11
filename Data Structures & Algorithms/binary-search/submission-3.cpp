21f8db78682e7b1e77bfd565780f573f001a95ee
PJ Yoo <pauljgy@gmail.com>
2026-04-11 16:30:27 -0400

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int mid;
+
+        while (l < r) {
+            mid = (l+r)/2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (target > nums[mid])
+                l = mid+1;
+            else
+                r = mid-1;
+        }
+        
+        return nums[l] == target ? l : -1;
+    }
+};
